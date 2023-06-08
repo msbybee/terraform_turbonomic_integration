0a1dedaa73a0c07a6c687e220fe595eb7f129ea2
msbybee <38099963+msbybee@users.noreply.github.com>
2023-06-08 12:17:54 -0500

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = ${aws_region_name}us-east-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0715c1897453cabd1"
+  instance_type = "${aws_instance_type}"
+
+  tags = {
+    Name = "terraTurboInstance"
+  }
+}
